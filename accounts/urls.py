f1110c03ec610d160c3c0334d506b9d836d55e09
3chammmpppthichanon <champ.thichanon@gmail.com>
2025-03-23 23:11:23 +0700

    update

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('login/', views.login, name='login'),
     path('logout/', views.logout, name='logout'),
+    path('project_list/', views.project_list, name='project_list'),
     path('projects/<int:project_id>/submissions/', SubmissionView.as_view(), name='submissions'),
     path('submissions/<int:submission_id>/download/', download_submission, name='download_submission'),
 ]
